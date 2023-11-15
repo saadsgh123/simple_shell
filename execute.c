9d70c757b759f250f7617ee68792232cc2595a63
saadsgh123 <sdsghouri@gmail.com>
2023-11-15 21:49:14 +0000

    compiler problem solved

--- execute.c
+++ execute.c
@@ -8,6 +8,7 @@
 int _execute(char **cmd, char **env, char **argv, int idx) {
     char *fullcmd = NULL;
     pid_t child;
+    int status;
     
     fullcmd = Hnadlepath(cmd[0], env);
     if (fullcmd == NULL) {
@@ -18,12 +19,11 @@ int _execute(char **cmd, char **env, char **argv, int idx) {
     child = fork();
     if (child == 0) {
         if (execve(fullcmd, cmd, env) == -1) {
-            printerror(argv[0], cmd[0], idx); // Print an error message
+            printerror(argv[0], cmd[0], idx);
             exit(127);
         }
     } else {
         free(fullcmd);
-        int status;
         wait(&status);
         return status;
     }
--- proceesinput.c
+++ proceesinput.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 void processInput(char *line, char **argv, char **env) {
-    int status = 0;
+    int status = 0, i, j;
     int idx = 0;
     char **command = NULL;
 
-    for (int i = 0; line[i] != '\0'; i++) {
+    for (i = 0; line[i] != '\0'; i++) {
         if (line[i] != ' ' && line[i] != '\t') {
             idx++;
             break;
@@ -24,8 +24,8 @@ void processInput(char *line, char **argv, char **env) {
             status = _execute(command, env, argv, idx);
         }
 
-        for (int i = 0; command[i] != NULL; i++) {
-            free(command[i]);
+        for (j = 0; command[j] != NULL; j++) {
+            free(command[j]);
         }
         free(command);
     }
